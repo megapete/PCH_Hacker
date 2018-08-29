88d1b3330fd088e26469c2aab1596decbd6ab199
Peter Huber <peter@huberistech.com>
2018-08-29 11:41:13 -0400

    Initial Commit

---
+++ PCH_Hacker/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  PCH_Hacker
+//
+//  Created by PeterCoolAssHuber on 2018-08-29.
+//  Copyright © 2018 Peter Huber. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
